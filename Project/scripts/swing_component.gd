f16c41d2a2fd3c730016695c755eb634dda49e0f
Derek Lane <wahoskendev@gmail.com>
2026-03-12 17:55:24 -0400

    dialed in swinging

--- Project/scripts/swing_component.gd
+++ Project/scripts/swing_component.gd
@@ -6,7 +6,9 @@ var is_swinging = false
 var pivot_point = Vector2.ZERO
 var swing_radius = 0.0
 var swing_angle = 0.0
-var swing_speed = 4
+var swing_speed = 5.0
+var swing_cooldown = 0.1  # seconds
+var cooldown_timer = 0.0
 var swing_direction = 1
 var min_angle = -1.2
 var max_angle = 1.2
@@ -19,28 +21,30 @@ func _ready() -> void:
 	player = get_parent()
 
 func _physics_process(delta):
-	
+	# Swing movement
+	if is_swinging:
+		swing_angle += swing_speed * delta * swing_direction
+		if swing_angle > max_angle:
+			swing_angle = max_angle
+			swing_direction *= -1
+		elif swing_angle < min_angle:
+			swing_angle = min_angle
+			swing_direction *= -1
 
-	if !is_swinging:
-		return
+		var x = pivot_point.x + cos(base_angle + swing_angle) * swing_radius
+		var y = pivot_point.y + sin(base_angle + swing_angle) * swing_radius
+		player.global_position = Vector2(x, y)
 
-	swing_angle += swing_speed * delta * swing_direction
-
-	if swing_angle > max_angle:
-		swing_angle = max_angle
-		swing_direction *= -1
-
-	if swing_angle < min_angle:
-		swing_angle = min_angle
-		swing_direction *= -1
-
-	var x = pivot_point.x + cos(base_angle + swing_angle) * swing_radius
-	var y = pivot_point.y + sin(base_angle + swing_angle) * swing_radius
-
-	player.global_position = Vector2(x, y)
+	# Countdown cooldown
+	if cooldown_timer > 0:
+		cooldown_timer -= delta
+		if cooldown_timer < 0:
+			cooldown_timer = 0
 	 
 
 func start_swing(pivot_position: Vector2):
+	if is_swinging or cooldown_timer > 0:
+		return  # cannot grab yet
 	is_swinging = true
 	pivot_point = pivot_position
 	swing_radius = max(30, player.global_position.distance_to(pivot_point) - swing_radius_offset)
@@ -56,19 +60,16 @@ func start_swing(pivot_position: Vector2):
 	player.velocity = Vector2.ZERO
 	
 func release_swing():
-
 	is_swinging = false
 
+	# Start cooldown
+	cooldown_timer = swing_cooldown
+
 	var launch_speed = 450
+	var launch_direction = Vector2(-sin(swing_angle), cos(swing_angle))
 
-	var launch_direction = Vector2(
-		-sin(swing_angle),
-		cos(swing_angle)
-	)
-
-	# add upward boost
+	# Add upward boost
 	launch_direction.y -= 0.4
-
 	launch_direction = launch_direction.normalized()
 
 	player.velocity = launch_direction * launch_speed
