587c2ec79c899dd518b78bb2bfbc08a6183143d3
Derek Lane <wahoskendev@gmail.com>
2026-03-12 21:48:54 -0400

    Ui scales on portrait correctly

--- Project/scripts/ui.gd
+++ Project/scripts/ui.gd
@@ -1,18 +1,21 @@
-extends Camera2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	adjust_zoom()
+	adjust_ui_scale()
 
-func adjust_zoom():
+
+func adjust_ui_scale():
 	var screen = get_viewport().get_visible_rect().size
 	
 	if screen.y > screen.x:
-		zoom = Vector2(2.5, 2.5)
+		# portrait
+		scale = Vector2(1.4, 1.4)
 	else:
-		zoom = Vector2(2.0, 2.0)
+		# landscape
+		scale = Vector2(1.0, 1.0)
 
 func _notification(what):
 	if what == NOTIFICATION_WM_SIZE_CHANGED:
-		adjust_zoom()
+		adjust_ui_scale()
