9e548a044bf14d054d4819f9f93df0818207c36a
Derek Lane <wahoskendev@gmail.com>
2026-05-12 21:30:42 -0400

    more stable pause menu dropdown

--- Project/scripts/pause_menu.gd
+++ Project/scripts/pause_menu.gd
@@ -2,38 +2,38 @@ extends CanvasLayer
 
 signal menu_closed
 
-@onready var nuts_label: Label = $Panel/VBoxContainer/NutsLabel
-@onready var cosmetic_option_button: OptionButton = $Panel/VBoxContainer/CosmeticOptionButton
-@onready var resume_button: Button = $Panel/VBoxContainer/ResumeButton
+@onready var nuts_label: Label = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/NutsLabel
+
+@onready var cosmetic_dropdown_button: Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/CosmeticDropdownButton
+@onready var cosmetic_options_container: VBoxContainer = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/CosmeticOptionsContainer
+
+@onready var default_button: Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/CosmeticOptionsContainer/DefaultButton
+@onready var acorn_cap_button: Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/CosmeticOptionsContainer/AcornCapButton
+@onready var super_squirrel_button: Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/CosmeticOptionsContainer/SuperSquirrelButton
+
+@onready var resume_button: Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/ResumeButton
 
 var is_closing := false
-var is_rebuilding_dropdown := false
+var dropdown_open := false
 
-var cosmetic_options := [
-	{
-		"id": "",
-		"name": "Default",
-		"owned": true
-	},
-	{
-		"id": "acorn_cap",
-		"name": "Acorn Cap",
-		"owned": false
-	},
-	{
-		"id": "super_squirrel",
-		"name": "Super Squirrel",
-		"owned": false
-	}
-]
+const DEFAULT_ID := ""
+const ACORN_CAP_ID := "acorn_cap"
+const SUPER_SQUIRREL_ID := "super_squirrel"
 
 
 func _ready() -> void:
-	cosmetic_option_button.item_selected.connect(_on_cosmetic_selected)
+	cosmetic_dropdown_button.pressed.connect(toggle_cosmetic_dropdown)
+
+	default_button.pressed.connect(func(): select_cosmetic(DEFAULT_ID))
+	acorn_cap_button.pressed.connect(func(): select_cosmetic(ACORN_CAP_ID))
+	super_squirrel_button.pressed.connect(func(): select_cosmetic(SUPER_SQUIRREL_ID))
+
 	resume_button.pressed.connect(close_menu)
 
+	cosmetic_options_container.hide()
+
 	update_inventory_display()
-	cosmetic_option_button.grab_focus()
+	cosmetic_dropdown_button.grab_focus()
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -41,85 +41,95 @@ func _unhandled_input(event: InputEvent) -> void:
 		return
 
 	if event.is_action_pressed("ui_cancel") or event.is_action_pressed("pause"):
-		close_menu()
+		if dropdown_open:
+			close_cosmetic_dropdown()
+		else:
+			close_menu()
+
 		get_viewport().set_input_as_handled()
 		return
 
 	if event.is_action_pressed("ui_accept"):
-		activate_focused_control()
+		activate_focused_button()
 		get_viewport().set_input_as_handled()
 		return
 
 
-func activate_focused_control() -> void:
+func activate_focused_button() -> void:
 	var focused := get_viewport().gui_get_focus_owner()
 
 	if focused is Button:
 		focused.emit_signal("pressed")
-	elif focused is OptionButton:
-		focused.show_popup()
+
+
+func toggle_cosmetic_dropdown() -> void:
+	if dropdown_open:
+		close_cosmetic_dropdown()
+	else:
+		open_cosmetic_dropdown()
+
+
+func open_cosmetic_dropdown() -> void:
+	dropdown_open = true
+	cosmetic_options_container.show()
+	update_inventory_display()
+
+	match GameState.equipped_cosmetic:
+		ACORN_CAP_ID:
+			acorn_cap_button.grab_focus()
+		SUPER_SQUIRREL_ID:
+			super_squirrel_button.grab_focus()
+		_:
+			default_button.grab_focus()
+
+
+func close_cosmetic_dropdown() -> void:
+	dropdown_open = false
+	cosmetic_options_container.hide()
+	cosmetic_dropdown_button.grab_focus()
+
+
+func select_cosmetic(item_id: String) -> void:
+	if item_id != "" and not GameState.owns_cosmetic(item_id):
+		return
+
+	if item_id == DEFAULT_ID:
+		GameState.equipped_cosmetic = DEFAULT_ID
+		GameState.cosmetic_equipped.emit(DEFAULT_ID)
+	else:
+		GameState.equip_cosmetic(item_id)
+
+	update_inventory_display()
+	close_cosmetic_dropdown()
 
 
 func update_inventory_display() -> void:
 	nuts_label.text = "Nuts: %d" % GameState.nuts
-	rebuild_cosmetic_dropdown()
 
+	match GameState.equipped_cosmetic:
+		ACORN_CAP_ID:
+			cosmetic_dropdown_button.text = "Cosmetic: Acorn Cap"
+		SUPER_SQUIRREL_ID:
+			cosmetic_dropdown_button.text = "Cosmetic: Super Squirrel"
+		_:
+			cosmetic_dropdown_button.text = "Cosmetic: Default"
 
-func rebuild_cosmetic_dropdown() -> void:
-	is_rebuilding_dropdown = true
+	default_button.text = "Default"
+	default_button.disabled = false
 
-	cosmetic_option_button.clear()
-
-	for option in cosmetic_options:
-		var item_id: String = str(option["id"])
-		var item_name: String = str(option["name"])
-
-		var owned: bool = option.get("owned", false) as bool
-
-		if GameState.owns_cosmetic(item_id):
-			owned = true
-
-		var display_name := item_name
-
-		if not owned:
-			display_name += " (Locked)"
-
-		cosmetic_option_button.add_item(display_name)
-		cosmetic_option_button.set_item_metadata(cosmetic_option_button.item_count - 1, item_id)
-
-	select_equipped_cosmetic()
-
-	is_rebuilding_dropdown = false
-
-
-func select_equipped_cosmetic() -> void:
-	for i in range(cosmetic_option_button.item_count):
-		var item_id := str(cosmetic_option_button.get_item_metadata(i))
-
-		if item_id == GameState.equipped_cosmetic:
-			cosmetic_option_button.select(i)
-			return
-
-	cosmetic_option_button.select(0)
-
-
-func _on_cosmetic_selected(index: int) -> void:
-	if is_rebuilding_dropdown:
-		return
-
-	var selected_id := str(cosmetic_option_button.get_item_metadata(index))
-
-	if selected_id != "" and not GameState.owns_cosmetic(selected_id):
-		select_equipped_cosmetic()
-		return
-
-	if selected_id == "":
-		GameState.equipped_cosmetic = ""
-		GameState.cosmetic_equipped.emit("")
+	if GameState.owns_cosmetic(ACORN_CAP_ID):
+		acorn_cap_button.text = "Acorn Cap"
+		acorn_cap_button.disabled = false
 	else:
-		GameState.equip_cosmetic(selected_id)
+		acorn_cap_button.text = "Acorn Cap (Locked)"
+		acorn_cap_button.disabled = true
 
-	update_inventory_display()
+	if GameState.owns_cosmetic(SUPER_SQUIRREL_ID):
+		super_squirrel_button.text = "Super Squirrel"
+		super_squirrel_button.disabled = false
+	else:
+		super_squirrel_button.text = "Super Squirrel (Locked)"
+		super_squirrel_button.disabled = true
 
 
 func close_menu() -> void:
